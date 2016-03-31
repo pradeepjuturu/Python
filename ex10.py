a5101ebfdf3b93b560c71ebe84655f13208b449b
Pradeep.Juturu <pradeep@pradeep.com>
2016-03-31 11:16:42 -0700

    Adding ex10 to repository

---
+++ ex10.py
@@ -0,0 +1,12 @@
+tabby_cat = "\t I'm tabbed in"
+persian_cat = "I'm split\Not a line"
+backslash_cat ="I'm\\not a\\cat"
+fat_cat = """
+I'll do a list
+\t *cat food
+\t *fishes
+\t *catnip\n \t* Grass"""
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
---
+++ ex9.py
@@ -0,0 +1,11 @@
+days = "Mon Tue Wed Thur Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun"
+
+print "Here are the days", days
+print "Here are the first six months", months
+
+print """I am not sure what three quotes does here
+I would like find out. I forgot how to get this executed in the ubuntu"""
+
+print "trying to add"
+print "just want to see"
